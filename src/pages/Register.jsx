fd0a4615e50e8487b5852973184cd80d67568646
Parimaladevi <parimaladevi898@gmail.com>
2026-06-20 20:33:03 +0530

    updated frontend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,69 @@
+import { useState } from "react";
+import API from "../services/api";
+
 function Register(){
 
-    return (
-        <h1>Register Page</h1>
-    )
+const [name,setName]=useState("");
+const [email,setEmail]=useState("");
+const [password,setPassword]=useState("");
+
+
+const registerUser = async(e)=>{
+
+e.preventDefault();
+
+try{
+
+const res = await API.post("/users/register",{
+name,
+email,
+password
+});
+
+alert(res.data.message);
+
+}
+catch(err){
+
+alert("Registration failed");
+
+}
+
+}
+
+
+return(
+<div>
+
+<h1>Register</h1>
+
+<form onSubmit={registerUser}>
+
+<input 
+placeholder="Name"
+onChange={(e)=>setName(e.target.value)}
+/>
+
+<input 
+placeholder="Email"
+onChange={(e)=>setEmail(e.target.value)}
+/>
+
+<input 
+placeholder="Password"
+type="password"
+onChange={(e)=>setPassword(e.target.value)}
+/>
+
+<button>
+Register
+</button>
+
+</form>
+
+</div>
+)
+
 }
 
 export default Register;
